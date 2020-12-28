0bd060aaec60d2526496cd74495b7b3fb42e14a1
Tatiana Anufrieva <anufri_ta@rambler.ru>
2020-12-28 12:45:26 +0400

    Fixed Router for Github Pages

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import "react-app-polyfill/ie11";
 import "react-app-polyfill/stable";
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./index.scss";
 import App from "./components/App";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
